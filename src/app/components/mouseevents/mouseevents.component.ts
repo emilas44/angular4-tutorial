536c69455d8fbbcbf75e4c5dcf92931d32a0333c
emilas44 <milas.elvis@gmail.com>
2017-08-02 09:21:53 +0200

    mouse events

--- src/app/components/mouseevents/mouseevents.component.ts
+++ src/app/components/mouseevents/mouseevents.component.ts
@@ -4,29 +4,46 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-mouseevents',
   template: `
       <h1>Mouse Events</h1>
-      <button id="element-id-name" class="btn btn-primary" (click)="clickEvent($event, 'Hello')">Click Event</button>
-      <button id="element-id-name" class="btn btn-primary" (mouseover)="mouseOverEvent($event, 'Hi')">Mouse Over Event</button>
+      <button id="element-id-name" class="btn btn-primary" (click)="fireEvent($event)">Click Event</button>
+      <button id="element-id-name" class="btn btn-primary" (mouseover)="fireEvent($event)">Mouse Over Event</button>
+      <button id="element-id-name" class="btn btn-primary" (mousedown)="fireEvent($event)">Mouse Down Event</button>
+      <button id="element-id-name" class="btn btn-primary" (mouseup)="fireEvent($event)">Mouse Up Event</button>
+      <button id="element-id-name" class="btn btn-primary" (dblclick)="fireEvent($event)">Mouse Double Click</button>
+      <hr>
+      <img src="http://lorempixel.com/50/50" id="element-id-name" class="btn btn-primary" (drag)="dragEvent($event)"/>
+      <img src="http://lorempixel.com/50/50" id="element-id-name" class="btn btn-primary" (dragover)="dragOverEvent($event)"/>
+      <h1>Changing properties on event</h1>
+      <button (click)="changeValue()">Change Value</button><span><p>{{ text }}</p></span>
   `
 })
 export class MouseeventsComponent implements OnInit {
+
+  text:string = 'Hello World';
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  clickEvent(e, greeting) {
-    console.log('Brutto clicked');
-    console.log(greeting)
-    console.log(e.clientY);
-    console.log(e.clientX);
-    console.log(e.target.id);
-    console.log(e.target.className);
+  fireEvent(e) {
+    //console.log(greeting);
+    //console.log(e.clientY);
+    //console.log(e.clientX);
+    //console.log(e.target.id);
+    //console.log(e.target.className);
     console.log(e.type);
   }
 
-  mouseOverEvent(e, greeting) {
-    console.log('Mouse is hovered');
+  dragEvent(e) {
+    console.log(e.type);
+  }
+
+  dragOverEvent(e) {
+    console.log(e.type);
+  }
+
+  changeValue() {
+    this.text = 'Hello World is Changed!'
   }
 
 }
